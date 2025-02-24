be189d949ceb6ee10cf0627de2ee1cc2080dfce3
damascussteel21 <tallbreadstick@gmail.com>
2025-02-24 16:43:38 +0800

    bruh!
    nothing

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::{get, post, delete},
+    extract::{Path, State, Query},
+    Router
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+
+#[tokio::main]
+async fn main() {
+    
+    
+
+}
